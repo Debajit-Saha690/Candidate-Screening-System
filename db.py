fbe14e96cbb158b43dc09aea08ddb0194d3919ce
Debajit-Saha690 <debajitsahacse@gmail.com>
2026-05-03 21:36:31 +0530

    Update ATS project with screenshots and improvements

--- db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector 
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Debajit@Saha1237890",
-        database="resume_system"
-    )
